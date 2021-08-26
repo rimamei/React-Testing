875a8aededa69ac6e2428bfcc8b158b8a9bd6d89
Rima <rimameih@gmail.com>
2021-08-27 01:20:06 +0700

    Enzyme setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import Enzyme from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import App from './App'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  
+})
