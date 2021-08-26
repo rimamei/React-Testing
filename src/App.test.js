b03df27fd26e2fba52ab54f8efd57097a469dbd3
Rima <rimameih@gmail.com>
2021-08-27 01:27:16 +0700

    Test dengan enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Enzyme from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 import App from './App'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-test('renders learn react link', () => {
-  
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+  console.log(wrapper.debug())
 })
