a19821944bb0a42624c401eb391b924fce4bd566
Rima <rimameih@gmail.com>
2021-08-28 22:18:25 +0700

    Props Testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
+import Counter from './Counter/counter'
+import Congrats from './Joto/Congrats/Congrats'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div data-test='component-app'>
-      <h1 data-test='counter-display'>
-        The counter is currently&nbsp;
-        <span data-test='count'>{count}</span>
-      </h1>
-      <button data-test='increment-button' onClick={() => setCount(count + 1)}>
-        Increment Counter
-      </button>
-      <button data-test='decrement-button' onClick={() => setCount(count - 1)}>
-        Decrement Counter
-      </button>
+    <div>
+      <Counter />
+      <Congrats />
     </div>
   )
 }
