c323452dc1767832a4f4effd8f8ae9ebfccdd7a1
Rima <rimameih@gmail.com>
2021-08-30 05:59:44 +0700

    Add Style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
-import Counter from './Counter/counter'
-import Congrats from './Joto/Congrats/Congrats'
+import Counter from './Counter/Counter'
+import Congrats from './Joto/Props/Congrats'
 import GuessedWords from './Joto/GuessedWords'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Counter />
-      <Congrats />
-      <GuessedWords guessedWords={'test'} />
+    <div className='container'>
+      <Counter count={count}/>
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+      />
     </div>
   )
 }
