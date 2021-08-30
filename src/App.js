7b2974f614d39bd2316ac5dcb9e934033aee099e
Rima <rimameih@gmail.com>
2021-08-30 10:25:16 +0700

    Not destructuring useState

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { useState } from 'react'
 import Counter from './Counter/Counter'
 import Congrats from './Joto/Props/Congrats'
 import GuessedWords from './Joto/GuessedWords'
+import Input from './Joto/Input'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='container'>
-      <Counter count={count}/>
+      <Counter count={count} />
       <h1>Jotto</h1>
       <Congrats success={true} />
       <GuessedWords
         guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
       />
+      <Input />
     </div>
   )
 }
---
+++ src/Joto/Input/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('')
+
+  return (
+    <div data-test='component-input'>
+      <form action='' className='form-inline'>
+        <input
+          data-test='input-box'
+          type='text'
+          className='mb-2 mx-sm-3'
+          placeholder='enter guess'
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test='submit-button'
+          className='btn btn-primary'
+          onClick={(e) => {
+            e.preventDefault()
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+}
+
+export default Input
